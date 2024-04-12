9bf9a6df5e58e1a06c02c2a76f5ba7295e6a9399
darkeny <darkenpaulo00@gmail.com>
2024-04-12 23:22:25 +0200

    update: cleaning code

--- VOID/src/controllers/postsController.ts
+++ VOID/src/controllers/postsController.ts
@@ -3,66 +3,99 @@ import { prisma } from '../database/prisma';
 
 const postsRouter = Router();
 
-// Route to find all posts in the system
+/**
+ * Route to find all posts 
+ */
 postsRouter.get('/posts', async (request, response) => {
     try {
         const post = await prisma.posts.findMany();
-    response.json(post);
+        response.json(post);
     } catch (error) {
         response.status(500).json({ error: 'Failed to retrieve post.' });
     }
 });
 
-// Route to create a post
+
+
+/**
+ * Route to create a post
+ */
 postsRouter.post('/posts', async (request, response) => {
     try {
-        const { title, content, published, userId } = request.body;
-        
-        // Verificar se o usuário existe
-        const userExists = await prisma.user.findUnique({
-            where: { id: userId }
-        });
 
-        if (!userExists) {
-            return response.status(404).json({ error: 'User not found.' });
-        }
-        const existingPost = await prisma.posts.findFirst({
-            where:{
-                userId: userId,
-                title: title,
+        if (Array.isArray(request.body)) {
+            for (const post of request.body) {
+                console.log(post.category);
+                if (post.category !== 'BUSINESS' && post.category !== 'EDUCATION' && post.category !== 'SPORTS') {
+                    return response.status(400).json({ message: 'Invalid Category' })
+                }    
             }
-        });
+            const count = await prisma.posts.createMany({
+                skipDuplicates: true,
+                data: request.body.map(({ title, content, published, category, userId }) => {
+                    return { title, content, published, category, userId }
+                })
+            })
 
-        if (existingPost){
-            return response.status(400).json({ error: 'Post already exists for this user.'})
+            response.status(201).json({count})
         }
 
-        
-        const post = await prisma.posts.create({
-            data: {
-                title,
-                content,
-                published,
-                User: { connect: { id: userId } }
+        else {
+            // Verificar se o usuário existe
+            const { title, content, published, category, userId } = request.body;
+
+            if (category !== 'BUSINESS' && category !== 'EDUCATION' && category !== 'SPORTS') {
+                return response.status(400).json({ message: 'Invalid Category' })
             }
-        });
-        response.json({
-            message: "Post Created",
-            payload: post
-        });
-    } catch (error) {
-        response.status(500).json({ error: 'Failed to create post.'});
+            const userExists = await prisma.user.findUnique({
+                where: { id: userId }
+            });
+
+            if (!userExists) {
+                return response.status(404).json({ error: 'User not found.' });
+            }
+            const existingPost = await prisma.posts.findUnique({
+                where: {
+                    title_userId: { userId, title },
+                }
+            });
+
+            if (existingPost) {
+                return response.status(400).json({ error: 'Post already exists for this user.' })
+            }
+
+
+            const post = await prisma.posts.create({
+                data: {
+                    title,
+                    content,
+                    published,
+                    category,
+                    User: { connect: { id: userId } }
+                }
+            });
+            response.json({
+                message: "Post Created",
+                payload: post
+            });
+        }
+
+    } catch (error: any) {
+        response.status(500).json({ error: 'Failed to create post.'  });
         console.log(error);
-        
+
     }
 });
 
-// Route to update a post
+
+/**
+ * Route to update a post
+ */
 postsRouter.put('/posts', async (request, response) => {
     try {
         const { title, content, userId } = request.body;
         const post = await prisma.posts.update({
-            where: { title_userId: {title, userId} },
+            where: { title_userId: { title, userId } },
             data: { content: content }
         });
         response.json({
@@ -74,12 +107,16 @@ postsRouter.put('/posts', async (request, response) => {
     }
 });
 
-// Route to delete a delete
+
+
+/**
+ * Route to delete a delete
+ */
 postsRouter.delete('/posts', async (request, response) => {
     try {
-        const { title, userId} = request.body;
+        const { title, userId } = request.body;
         const post = await prisma.posts.delete({
-            where: { title_userId: {title, userId}}
+            where: { title_userId: { title, userId } }
         });
         response.json({
             message: "Post Deleted",
@@ -88,7 +125,7 @@ postsRouter.delete('/posts', async (request, response) => {
     } catch (error) {
         response.status(500).json({ error: 'Failed to delete post.' });
         console.log(error);
-        
+
     }
 });
 
